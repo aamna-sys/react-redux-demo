9310485451b5a88a1aa1ab575086f64c95e7f7cd
aamna-sys <aamna.sayyed@systemsltd.com>
2021-12-03 12:56:23 +0500

    Add h1 and hr

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -5,6 +5,8 @@ import { connect } from "react-redux";
 function CakeContainer(props) {
   return (
     <div>
+      <p>Redux Cake Shop</p>
+      <hr />
       <h2>Number of Cakes - {props.numOfCakes}</h2>
       <button onClick={props.buyCake}>Buy Cakes</button>
     </div>
